33d8c896cd238e3f24e27770e1936b87cd062bfd
Roman <roman.boxes@gmail.com>
2026-05-18 18:22:24 +0200

    Dockerfile implementation
    Dockerfile implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:3001");
+const socket = io(import.meta.env.VITE_API_URL);
 
 function App() {
   const [messages, setMessages] = useState([]);
