19c0b89101ff75e39ebf5b4737ded519d34d6a95
Roman <roman.boxes@gmail.com>
2026-05-18 22:42:04 +0200

    Update index.js and added .env
    Update index.js and added .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+import dotenv from "dotenv"; 
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -15,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: FRONTEND_URL,
     methods: ["GET", "POST"]
   }
 });
